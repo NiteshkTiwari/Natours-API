ea537528daab212b763d2a2cfb1363f7410c6747
NiteshkTiwari <nt08802@gmail.com>
2023-08-22 13:04:53 +0530

    Imported review and user data and also implemented indexing for our data model

--- models/tourModel.js
+++ models/tourModel.js
@@ -112,6 +112,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationInWeeks').get(function () {
   return this.duration / 7;
 });
